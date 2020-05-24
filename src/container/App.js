9ef004d8a51300024820319aaa12abfa3c8a10df
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-05-24 23:20:59 +0530

    [SETUP/PACKAGE] File Structure 🔥

---
+++ src/container/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'styles/index.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello Information
+            </div>
+        );
+    }
+}
+
+export default App;
